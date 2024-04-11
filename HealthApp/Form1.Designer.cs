45d34ce8d98184e8ae0eb8e16e3287a07951f4f4
Daniel Antonie <st10186731@vcconnect.edu.za>
2024-04-11 08:23:25 +0200

    this is a commit to for Prog 3rd year 1st semester POE application guide

---
+++ HealthApp/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace HealthApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtIDNum = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPasswrod = new System.Windows.Forms.TextBox();
+            this.txtConfPassword = new System.Windows.Forms.TextBox();
+            this.txtSpeciality = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PaleGreen;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(681, 915);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(735, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 45);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(735, 529);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 45);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(735, 790);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(187, 45);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Speciality : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(735, 401);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 45);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Email :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(735, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(180, 45);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Surname : ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(735, 284);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(201, 45);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "ID Number :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calibri", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(735, 655);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(317, 45);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Confirm Password : ";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(1397, 821);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(191, 90);
+            this.btnRegister.TabIndex = 8;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(1064, 53);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(245, 44);
+            this.txtName.TabIndex = 9;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(1064, 163);
+            this.txtSurname.Multiline = true;
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(245, 45);
+            this.txtSurname.TabIndex = 10;
+            // 
+            // txtIDNum
+            // 
+            this.txtIDNum.Location = new System.Drawing.Point(1064, 284);
+            this.txtIDNum.Multiline = true;
+            this.txtIDNum.Name = "txtIDNum";
+            this.txtIDNum.Size = new System.Drawing.Size(245, 45);
+            this.txtIDNum.TabIndex = 11;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(1064, 404);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(245, 42);
+            this.txtEmail.TabIndex = 12;
+            // 
+            // txtPasswrod
+            // 
+            this.txtPasswrod.Location = new System.Drawing.Point(1064, 530);
+            this.txtPasswrod.Multiline = true;
+            this.txtPasswrod.Name = "txtPasswrod";
+            this.txtPasswrod.Size = new System.Drawing.Size(245, 44);
+            this.txtPasswrod.TabIndex = 13;
+            // 
+            // txtConfPassword
+            // 
+            this.txtConfPassword.Location = new System.Drawing.Point(1093, 655);
+            this.txtConfPassword.Multiline = true;
+            this.txtConfPassword.Name = "txtConfPassword";
+            this.txtConfPassword.Size = new System.Drawing.Size(216, 45);
+            this.txtConfPassword.TabIndex = 14;
+            // 
+            // txtSpeciality
+            // 
+            this.txtSpeciality.Location = new System.Drawing.Point(1093, 790);
+            this.txtSpeciality.Multiline = true;
+            this.txtSpeciality.Name = "txtSpeciality";
+            this.txtSpeciality.Size = new System.Drawing.Size(216, 46);
+            this.txtSpeciality.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1647, 939);
+            this.Controls.Add(this.txtSpeciality);
+            this.Controls.Add(this.txtConfPassword);
+            this.Controls.Add(this.txtPasswrod);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtIDNum);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtIDNum;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPasswrod;
+        private System.Windows.Forms.TextBox txtConfPassword;
+        private System.Windows.Forms.TextBox txtSpeciality;
+    }
+}
+
---
+++ HealthApp/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HealthApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            //register implementaiton 
+            if (//validate method)
+            {
+                string name = txtName.Text;
+            }
+        }
+    }
+}
